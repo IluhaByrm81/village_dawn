1863cc1da3f43bc392fb6d11340e9edb0919ce71
IluhaByrm81 <iluhaburm@yandex.ru>
2021-09-15 11:59:26 +0300

    delete node modules

--- Backend/VillageDawn.sql
+++ Backend/VillageDawn.sql
@@ -1,10 +1,19 @@
 CREATE DATABASE VillageDawn;
 USE VillageDawn;
+
+DROP TABLE Users;
+
 CREATE TABLE Users (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(100) NOT NULL,
-  email VARCHAR(100),
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `username` VARCHAR(100) NOT NULL,
+  `lot_number` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(100) NOT NULL
 );
+
+INSERT INTO Users (username, lot_number, email, password)
+VALUES ("Alex", "48","qwert@qa", "12345qwert");
+
 SELECT
   *
 FROM
